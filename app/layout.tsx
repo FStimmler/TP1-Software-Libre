200b5bd05b7ab530c66038dcd6a972f93e4f0b10
FStimmler <franciscostimmler@yahoo.com.ar>
2025-06-19 21:12:38 +0000

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning={true}>
       <head>
-        <link rel="icon" href="/cow-icon.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       </head>
       <body className={inter.className} suppressHydrationWarning={true}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
